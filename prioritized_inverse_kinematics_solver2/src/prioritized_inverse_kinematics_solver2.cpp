3aa400574457aa07658eccdabc3f7dd7e5032493
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2025-11-24 18:02:32 +0900

    fix

--- prioritized_inverse_kinematics_solver2/src/prioritized_inverse_kinematics_solver2.cpp
+++ prioritized_inverse_kinematics_solver2/src/prioritized_inverse_kinematics_solver2.cpp
@@ -222,22 +222,6 @@ namespace prioritized_inverse_kinematics_solver2 {
       double time = timer.measure();
       std::cerr << "[PrioritizedIK] solveIKOnce time: " << time << "[s]. norm: " << result.norm() << std::endl;
     }
-    if(param.debugLevel>2 && param.viewer) {
-      std::vector<cnoid::SgNodePtr> markers;
-      for(int i=0;i<ikc_list.size();i++){
-        for(int j=0;j<ikc_list[i].size();j++){
-          const std::vector<cnoid::SgNodePtr>& marker = ikc_list[i][j]->getDrawOnObjects();
-          std::copy(marker.begin(), marker.end(), std::back_inserter(markers));
-        }
-      }
-      param.viewer->drawOn(markers);
-      param.viewer->drawObjects();
-      if (param.viewMilliseconds>=0) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(param.viewMilliseconds));
-      } else {
-        getchar();
-      }
-    }
 
     return result.norm() < param.convergeThre;
   }
@@ -370,6 +354,23 @@ namespace prioritized_inverse_kinematics_solver2 {
         (*it)->calcCenterOfMass();
       }
       updateConstraints(variables, ikc_list, rejections, param);
+
+      if(param.debugLevel>2 && param.viewer) {
+        std::vector<cnoid::SgNodePtr> markers;
+        for(int i=0;i<ikc_list.size();i++){
+          for(int j=0;j<ikc_list[i].size();j++){
+            const std::vector<cnoid::SgNodePtr>& marker = ikc_list[i][j]->getDrawOnObjects();
+            std::copy(marker.begin(), marker.end(), std::back_inserter(markers));
+          }
+        }
+        param.viewer->drawOn(markers);
+        param.viewer->drawObjects();
+        if (param.viewMilliseconds>=0) {
+          std::this_thread::sleep_for(std::chrono::milliseconds(param.viewMilliseconds));
+        } else {
+          getchar();
+        }
+      }
 
       if(path != nullptr && (loop + 1) % param.pathOutputLoop == 0) {
         path->resize(path->size() + 1);
